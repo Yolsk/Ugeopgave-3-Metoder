c680bdddbcd4d453ffbb421621ae2485f3e7538c
jakob <cph-jc341@stud.ek.dk>
2026-09-16 17:21:16 +0200

    Opgave 1, 2 og 3

--- Ugeopgave 3/src/myClass.java
+++ Ugeopgave 3/src/myClass.java
@@ -2,14 +2,99 @@ public class myClass {
 
 
 
+    // opgave 3: Pris-beregner
 
-void main() {
+     double applyDiscount(double price, double discountPercent) {
+         double discountPrice = price / 100 * discountPercent;
+         return price - discountPrice;
 
-    // opgave 1
+     }
+
+     double addTax(double price) {
+         double tax = price *0.25;
+         return price + tax;
+
+     }
+
+     double calculateFinalPrice (double basePrice, double discount) {
+         double discountedPrice = applyDiscount(basePrice, discount);
+         return addTax(discountedPrice);
+
+     }
+     void main () {
+
+         double finalPrice = calculateFinalPrice(500, 20);
+         System.out.println("Final Price: " + finalPrice);
+
+     }
 
 
 
+
+
+
+/*
+
+    // opgave 1 Bank-konto
+    double balance = 0;
+    String accountName;
+
+    void deposit100() {
+        balance+= 100;
+    }
+
+    void withdraw50() {
+        balance -= 50;
+    }
+
+    void printBalance() {
+        System.out.println(accountName + " has " + balance + " kr");
+
+    }
+
+    void main()  {
+        accountName = "Jakob";
+        deposit100();
+        deposit100();
+        withdraw50();
+        printBalance();
 
 
     }
+     // opgave 2 Karakter-beregner
+
+     int assignementsPoints;
+     int examPoints;
+     int projectPoints;
+
+
+     void addAssignementPoints(int points) {
+         assignementsPoints+= points;
+
+     }
+
+     void addExamPoints(int points) {
+         examPoints+= points;
+
+     }
+
+     void addProjectPoints(int points) {
+         projectPoints+= points;
+
+     }
+
+     void printTotal() {
+         int total = assignementsPoints + examPoints + projectPoints;
+         System.out.println(total);
+
+     }
+
+ */
+
+
+
+
+
+
 }
+
