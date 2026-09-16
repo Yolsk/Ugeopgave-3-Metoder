9b8386f8fd572602bf6dc37f9bf43f22626fcb2d
jakob <cph-jc341@stud.ek.dk>
2026-09-16 18:20:28 +0200

    Opgave 1, 2, 3 og 4.

--- Ugeopgave 3/src/myClass.java
+++ Ugeopgave 3/src/myClass.java
@@ -1,36 +1,6 @@
 public class myClass {
 
-
-
-    // opgave 3: Pris-beregner
-
-     double applyDiscount(double price, double discountPercent) {
-         double discountPrice = price / 100 * discountPercent;
-         return price - discountPrice;
-
      }
-
-     double addTax(double price) {
-         double tax = price *0.25;
-         return price + tax;
-
-     }
-
-     double calculateFinalPrice (double basePrice, double discount) {
-         double discountedPrice = applyDiscount(basePrice, discount);
-         return addTax(discountedPrice);
-
-     }
-     void main () {
-
-         double finalPrice = calculateFinalPrice(500, 20);
-         System.out.println("Final Price: " + finalPrice);
-
-     }
-
-
-
-
 
 
 /*
@@ -86,6 +56,89 @@ public class myClass {
      void printTotal() {
          int total = assignementsPoints + examPoints + projectPoints;
          System.out.println(total);
+
+     }
+
+     // opgave 3: Pris-beregner
+
+     double applyDiscount(double price, double discountPercent) {
+         double discountPrice = price / 100 * discountPercent;
+         return price - discountPrice;
+
+     }
+
+     double addTax(double price) {
+         double tax = price *0.25;
+         return price + tax;
+
+     }
+
+     double calculateFinalPrice (double basePrice, double discount) {
+         double discountedPrice = applyDiscount(basePrice, discount);
+         return addTax(discountedPrice);
+
+     }
+     void main () {
+
+         double finalPrice = calculateFinalPrice(500, 20);
+         System.out.println("Final Price: " + finalPrice);
+
+     }
+     // opgave 4: Statistik-beregner
+
+    double calculateAverage(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum+= numbers[i];
+
+        }
+        return sum / numbers.length;
+    }
+
+    int findMax(int[] numbers) {
+        int highestNum = numbers[0];
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] > highestNum) {
+                highestNum = numbers[i];
+            }
+
+        }
+        return highestNum;
+    }
+
+    int findMin(int[] numbers) {
+        int lowestNum = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < lowestNum) {
+                lowestNum = numbers[i];
+            }
+
+        }
+        return lowestNum;
+    }
+
+    int countAboveAverage(int[] numbers) {
+        double average = calculateAverage(numbers);
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > average) {
+                counter++;
+            }
+
+        }
+        return counter;
+    }
+
+    void main () {
+        int[] numbers = {45, 67, 23, 89, 34, 56, 78};
+        double average = calculateAverage(numbers);
+        System.out.println("Average: " + average);
+        int highestNum = findMax(numbers);
+        System.out.println("Highest number: " + highestNum);
+        int lowestNum = findMin(numbers);
+        System.out.println("Lowest number: " + lowestNum);
+        int aboveAverage = countAboveAverage(numbers);
+        System.out.println("Above average: " + aboveAverage);
 
      }
 
